8072898b1eefff5187451fc556630f8e884d5dc5
Huynh Duc Duy <duy.huynh@linecorp.com>
2021-04-18 22:37:05 +0700

    configuration for the project

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  endOfLine: 'lf',
+  printWidth: 80,
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  semi: false,
+}
